798724bcf81aa51bb488ccbdf4511fba05740b2d
Carl August Gjørsvik <Carl.Gjorsvik@student.uib.no>
2019-03-31 14:17:43 +0200

    LZW ok, working on calculating compression rate

--- src/compression/LZW.java
+++ src/compression/LZW.java
@@ -1,31 +1,143 @@
 package compression;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 
+import sun.util.BuddhistCalendar;
+
 public class LZW {
 
-    private char[] chars;
-    private HashSet<Character> alphabet;
     private int ALPH_SIZE;
-    private HashMap<Integer, String> dictionary;
     private String text;
+    HashSet<Character> alphabet;
+    private BiDirectionalMap dictionary; // the dictionary used for compression and decompression
+    private ArrayList<Integer> compressed; //the output of the compression algorithm
+    private HashMap<Integer, Integer> bitCount; //stores the number of outputs with x as bitlength
+    private int currentBitLength;
+    private int initialBitLength;
+    private int dictionaryIndex;
     
-    public LZW(String source, char[] chars, HashSet<Character> alphabet, int alph_size) {
-        this.text = source;
-        this.chars = chars;
-        this.alphabet = alphabet;
+    public LZW(char[] chars, HashSet<Character> alphabet, int alph_size) {
         this.ALPH_SIZE = alph_size;
-        dictionary = new HashMap<>();
+        this.alphabet = alphabet;
+        dictionary = new BiDirectionalMap();
+        compressed = new ArrayList<>();
+        bitCount = new HashMap<>();
         
-        int x = 0;
-        for(Character c : alphabet) {
-            dictionary.put(x++, c.toString());
+        StringBuilder sb = new StringBuilder();
+        
+        dictionaryIndex=0;
+        for (char c : alphabet) {
+            System.out.println("adding character '" + c + "' to dictionary at index " + dictionaryIndex);
+            dictionary.put(dictionaryIndex++, Character.toString(c));
         }
-        System.out.println("initial text: " + source);
-        text.replaceAll("[^abcdefghijklmnopqrstuvwxyzæøå]", "");
+        currentBitLength = findInitialBitLength();
+        bitCount.put(currentBitLength, 0);
         
-        System.out.println("cleaned test: " + text);
+        for (int i=0; i<chars.length; i++) {
+            if (alphabet.contains(chars[i])) sb.append(chars[i]);
+        }
+        
+        text = sb.toString();
+    }
+    
+    public LZW(String source, HashSet<Character> alphabet, int alph_size) {
+        this.ALPH_SIZE = alph_size;
+        this.alphabet = alphabet;
+        this.text = source;
+        dictionary = new BiDirectionalMap();
+        compressed = new ArrayList<>();
+        bitCount = new HashMap<>();
+        
+        dictionaryIndex=0;
+        for (char c : alphabet) {
+            System.out.println("adding character '" + c + "' to dictionary at index " + dictionaryIndex);
+            dictionary.put(dictionaryIndex++, Character.toString(c));
+        }
+        currentBitLength = findInitialBitLength();
+        bitCount.put(currentBitLength, 0);
+    }
+    
+    public void compress() {
+        Integer pos = 0;
+        
+        for (int i=1; i<text.length(); i++) {
+            if (dictionary.contains(text.substring(pos, i+1)) == null) { //if current sequence + char at i is not in dictionary
+                compressed.add(dictionary.contains(text.substring(pos, i))); //output current sequence's index
+                dictionary.put(dictionaryIndex++, text.substring(pos, i+1)); //add current sequence + char to dictionary
+                System.out.println("adding sequence '" + text.substring(pos, i+1) + "' to dictionary at index " + (dictionaryIndex-1));
+                if (dictionary.size() >= Math.pow(2.0, currentBitLength)) { // increase bitlength if necessary
+                    currentBitLength++;
+                    bitCount.put(currentBitLength, 0);
+                }
+                bitCount.put(currentBitLength, bitCount.get(currentBitLength)+1); // log that an index was added to output of current bitlength
+                pos = i;
+            }
+        }
+        compressed.add(dictionary.contains(text.substring(pos, text.length())));
+        if (dictionary.size() >= Math.pow(2.0, currentBitLength)) {
+            currentBitLength++;
+            bitCount.put(currentBitLength, 0);
+        }
+        bitCount.put(currentBitLength, bitCount.get(currentBitLength)+1); // log that an index was added to output of current bitlength
+    }
+    
+    public void printCompressed() {
+        for (int i=0; i<compressed.size(); i++) {
+            System.out.print(compressed.get(i) + " ");
+        }
+        System.out.println();
+    }
+    
+    public void printCompressionRatio() {
+        System.out.println("initial bit length: " + initialBitLength);
+        System.out.println("text.length: " + text.length());
+        int UncompressedLength = text.length()*initialBitLength;
+        int compressedLength = 0;
+        for (int i=0; i<bitCount.size(); i++) {
+            if (bitCount.containsKey(i)) {
+                compressedLength += i*bitCount.get(i);
+            }
+        }
+        
+        System.out.println("Uncompressed bit-length: " + UncompressedLength);
+        System.out.println("Compressed bit-length: " + compressedLength);
+        
+    }
+    
+    private int findInitialBitLength() {
+        initialBitLength = 1;
+        int x = 1;
+        while (alphabet.size() > Math.pow(initialBitLength, 2)) initialBitLength = (int)Math.pow(2, x++);
+        return initialBitLength;
+    }
+    
+    private class BiDirectionalMap {
+        private HashMap<Integer, String> intToString;
+        private HashMap<String, Integer> stringToInt;
+        
+        public BiDirectionalMap() {
+            intToString = new HashMap<>();
+            stringToInt = new HashMap<>();
+        }
+        
+        public void put(int i, String s) {
+            intToString.put(i, s);
+            stringToInt.put(s, i);
+        }
+        
+        public Integer contains(String s) {
+            return stringToInt.get(s);
+        }
+        
+        public String get(int i) {
+            return intToString.get(i);
+        }
+        
+        public int size() {
+            return intToString.size();
+        }
     }
 
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -6,78 +6,78 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashSet;
-import java.util.Scanner;
 
 import compression.LZW;
 import markovModel.Markov;
 
 public class Main {
 
-    private static char[] chars = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'æ', 'ø', 'å', ' '};
+//    private static char[] chars = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+//            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'æ', 'ø', 'å', ' '};
+  private static char[] chars = {'#', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+  'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
     private static HashSet<Character> alphabet;
-    private final static int DICT_SIZE = 30;
+    private final static int DICT_SIZE = 27;
+    private static char[] sourceText;
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File f = new File("Folktale.html");
+        sourceText = new char[9000];
         
         alphabet = new HashSet<Character>();
         for (int i=0; i<DICT_SIZE; i++) {
             alphabet.add(chars[i]);
         }
         
-        Markov m = new Markov(f, chars, alphabet, 30);
-        int order = 0, length = 0;
-        String[] inSplit;
+//        Markov m = new Markov(f, chars, alphabet, 30);
         
-        Scanner sc = new Scanner(System.in);
-        
-        Scanner fsc;
+        BufferedReader br;
         try {
-            fsc = new Scanner(new FileReader(f));
-            StringBuilder sb = new StringBuilder();
-            String s = "";
-            while (fsc.hasNextLine()) {
-                s = fsc.nextLine();
-                System.out.println(s);
-                sb.append(s);
-            }
+            br = new BufferedReader(new FileReader(f));
+            int x = 0;
+            while(br.ready()) 
+                sourceText[x++] = ((char)br.read());
             
-            System.out.println(sb);
-            
-            new LZW(sb.toString(), chars, alphabet, DICT_SIZE);
-        } catch (FileNotFoundException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
+            br.close();
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();    
         }
         
-        
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        
+//        bw.write(sourceText, 0, sourceText.length);
+//        bw.flush();
+//        bw.close();
 //        m.entropy0();
 //        m.entropy1();
         
-        while(true) {
-            System.out.println("q = quit\n" 
-                    + "enter two space-separated integers;\nthe desired order followed by length of generated text.\n");
-            String input = sc.nextLine();
-            if (input.equals("q")) {
-                sc.close();
-                break;
-            }
-            
-            try {
-                inSplit = input.split(" ");
-                order = Integer.parseInt(inSplit[0]);
-                length = Integer.parseInt(inSplit[1]);
-                
-            } catch (Exception e) {
-                System.out.println("Unable to parse input.");
-            }
-            
-            m.generateOrder(length, order);
-        }
+        LZW testc = new LZW("tobeornottobeortobeornot#", alphabet, DICT_SIZE);
+        testc.compress();
+        testc.printCompressed();
+        testc.printCompressionRatio();
+        
+//        while(true) {
+//            System.out.println("q = quit\n" 
+//                    + "enter two space-separated integers;\nthe desired order followed by length of generated text.\n");
+//            String input = sc.nextLine();
+//            if (input.equals("q")) {
+//                sc.close();
+//                break;
+//            }
+//            
+//            try {
+//                inSplit = input.split(" ");
+//                order = Integer.parseInt(inSplit[0]);
+//                length = Integer.parseInt(inSplit[1]);
+//                
+//            } catch (Exception e) {
+//                System.out.println("Unable to parse input.");
+//            }
+//            
+//            m.generateOrder(length, order);
+//        }
         
     }
 
